cfda8c37c60e8049a45048740b1b421c200c5f23
akgmarquez <akgmarquez@gmail.com>
2018-07-13 18:40:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,50 @@
 class Song
-  @@count = 0
-  @@artists = []
-  @@genres = []
-  
-  def self.count
-    @@count
-  end
-  
-  def self.genres
-    @@genres.uniq
-  end
-  
-  def 
-  
-  attr_accessor :name, :artist, :genre
-  
-ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
- 
-ninety_nine_problems.name
-  # => "99 Problems"
- 
-ninety_nine_problems.artist
-  # => "Jay-Z"
- 
-ninety_nine_problems.genre
-  # => "rap"
-  
+ @@count = 0
+ @@artists = []
+ @@genres = []
+
+ def self.count
+   @@count
+ end
+
+ def self.genres
+   @@genres.uniq
+ end
+
+ def self.artists
+   @@artists.uniq
+ end
+
+ def self.genre_count
+   @@genre_1 = {}
+   @@genres.each do |genre|
+     @@genre_1[genre] ||= 0
+     @@genre_1[genre] += 1
+   end
+   @@genre_1
+ end
+
+ def self.artist_count
+   @@artist_1 = {}
+   @@artists.each do |artist|
+     @@artist_1[artist] ||= 0
+     @@artist_1[artist] += 1
+   end
+   @@artist_1
+ end
+
+ attr_accessor :name, :artist, :genre
+
+ def initialize(name, artist, genre)
+   @name = name
+   @artist = artist
+   @genre = genre
+   @@count += 1
+   @@genres << self.genre
+   @@artists << self.artist
+ end
+end
+Message Input
+
+
+Message Ashyah, Isabella Demetz, Olivia
