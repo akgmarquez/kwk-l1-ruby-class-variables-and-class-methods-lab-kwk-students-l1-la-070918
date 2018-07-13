af0f2c7e99bcc4544cbeba331f4076be9fc1add2
akgmarquez <akgmarquez@gmail.com>
2018-07-13 18:15:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,23 @@
-
+class Song
+  attr_accessor :name, :artist, :genre
+ 
+  def initialize
+    @@count == 0
+    @@count += 1
+  end
+  
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+ 
+ninety_nine_problems.name
+  # => "99 Problems"
+ 
+ninety_nine_problems.artist
+  # => "Jay-Z"
+ 
+ninety_nine_problems.genre
+  # => "rap"
+  
+  
+  
+  def ninety_nine_problems.genre
+  
